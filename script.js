3a74ae4dd102083d2f20e9d8630506af78d9c49a
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-02-01 15:24:40 +0530

    Menu Toggle Completed

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ var chk = document.querySelector("#dark-chk");
 chk.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 });
+
+// Toggle the submenu when clicking on the button
+
+var menuBtn = document.querySelector("#icon");
+var menu = document.querySelector("#submenu");
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("open-menu");
+});
