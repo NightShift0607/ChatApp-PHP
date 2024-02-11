4107acac1962914ec5b5adb669c986015bd18d1a
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-02-11 14:30:45 +0530

    Extra Change

--- php/users.php
+++ php/users.php
@@ -4,10 +4,10 @@
     $sender_id = $_SESSION['unique_id'];
     $query = mysqli_query($conn,"SELECT * FROM users WHERE NOT unique_id = {$sender_id}");
     $output = "";
-    if (mysqli_num_rows($query) == 1) {
+    if (mysqli_num_rows($query) == 0) {
         $output .= "No users are available to chat";
-    } elseif (mysqli_num_rows($query) > 1) {
-        include_once "data.php";
+    } elseif (mysqli_num_rows($query) > 0) {
+        include_once "data.php";   
     }
     echo $output;
 
