172686da71cdc17d8873feec60defec3e207e4a8
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-04-03 13:12:15 +0530

    Landing Page Added

--- index.php
+++ index.php
@@ -1,55 +1,21 @@
-<?php 
-  session_start();
-  if (isset($_SESSION['unique_id'])) {
-    header("location: users.php");  // if session is not set then go directly to login page
-  }
-?>
-
-<?php 
-include_once "header.php"; 
-include_once "dark-style.php"; 
-?>
+<?php include_once "header.php"; ?>
 
   <body>
     <div class="wrapper">
-      <section class="form signup">
-        <header>
-          <h1>ChitChatSphere</h1>
-          <label id="dark-label">
-            <input type="checkbox" id="dark-chk" />
-            <i class="fa-solid fa-sun sun"></i>
-            <i class="fa-solid fa-moon moon"></i>
-            <span class="toggle"></span>
-          </label>
-        </header>
-        <form action="#" enctype="multipart/form-data">
-          <div class="error-txt"></div>
-          <div class="field input">
-            <label>Username</label>
-            <input type="text" name="username" placeholder="Username" />
-          </div>
-          <div class="field input">
-            <label>Email Address</label>
-            <input type="text" name="email" placeholder="Enter your Email" />
-          </div>
-          <div class="field input">
-            <label>Password</label>
-            <input type="password" name="password" placeholder="Enter new Password" />
-            <i class="fa-solid fa-eye"></i>
-          </div>
-          <div class="field image">
-            <label>Select Image</label>
-            <input type="file" name="image" />
-          </div>
-          <div class="field button">
-            <input type="submit" value="Continue to Chat" />
-          </div>
-        </form>
-        <div class="link">Already signed up? <a href="login.php">Login now</a></div>
+      <section class="users">
+        <h1 id="landing-heading">
+          Welcome To <span id="chit">ChitChatSphere</span>
+        </h1>
+        <a href="signup.php"
+          ><div id="create-btn">Create a New Account</div>
+        </a>
+        <div id="or-container"><p id="or">or</p></div>
+        <a href="login.php"><div id="log-btn">Login</div></a>
+        <p id="para">To Start Chatting Now!!</p>
       </section>
     </div>
     <script src="JS/dark-mode.js"></script>
     <script src="JS/password-show.js"></script>
-    <script src="JS/signup.js"></script>
+    <script src="JS/login.js"></script>
   </body>
 </html>
--- signup.php
+++ signup.php
@@ -6,13 +6,13 @@
 ?>
 
 <?php 
-include_once "header.php";
+include_once "header.php"; 
 include_once "dark-style.php"; 
 ?>
 
   <body>
     <div class="wrapper">
-      <section class="form login">
+      <section class="form signup">
         <header>
           <h1>ChitChatSphere</h1>
           <label id="dark-label">
@@ -22,26 +22,34 @@ include_once "dark-style.php";
             <span class="toggle"></span>
           </label>
         </header>
-        <form action="#">
+        <form action="#" enctype="multipart/form-data">
           <div class="error-txt"></div>
+          <div class="field input">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
           <div class="field input">
             <label>Email Address</label>
             <input type="text" name="email" placeholder="Enter your Email" />
           </div>
           <div class="field input">
             <label>Password</label>
-            <input type="password" name="password" placeholder="Enter your Password" />
+            <input type="password" name="password" placeholder="Enter new Password" />
             <i class="fa-solid fa-eye"></i>
+          </div>
+          <div class="field image">
+            <label>Select Image</label>
+            <input type="file" name="image" />
           </div>
           <div class="field button">
             <input type="submit" value="Continue to Chat" />
           </div>
         </form>
-        <div class="link">Not yet signed up? <a href="signup.php">Signup now</a></div>
+        <div class="link">Already signed up? <a href="login.php">Login now</a></div>
       </section>
     </div>
     <script src="JS/dark-mode.js"></script>
     <script src="JS/password-show.js"></script>
-    <script src="JS/login.js"></script>
+    <script src="JS/signup.js"></script>
   </body>
 </html>
